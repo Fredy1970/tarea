939b01023a7cf3c17b19b65f70056922c78d312a
laqv1970 <lquinonezv1970@gmail.com>
2020-02-09 18:35:18 -0600

    Agregando programa universidad

---
+++ progconsola.js
@@ -0,0 +1,5 @@
+var nombre = "Luis Alfredo Quiñonez Valdez";
+var carnet = "20013449";
+var dato = prompt("Ingresa un dato: ");
+console.log("Tu dato es: " + dato);
+console.log("Nombre: " + nombre + " " + "Carnet:" + carnet);
